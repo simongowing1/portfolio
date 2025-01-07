74b13357a4bbc1781a538e3092ea2e801cfb23ba
simongowing1 <simongowing1@gmail.com>
2025-01-07 19:17:06 +0100

    refactor: home page

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -1,31 +1,15 @@
 import { Suspense } from "react";
-import Link from "next/link";
 
 import { AllProjects } from "@/app/components/global/Projects";
-import GetStartedCode from "@/app/components/global/GetStartedCode";
 
 export default async function Page() {
   return (
     <>
-      <div className="bg-gradient-to-r from-red-200 from-0% via-white via-40%  relative">
+      <div className="h-screen bg-gradient-to-r from-red-200 from-0% via-white via-40%  relative">
         <div className="bg-gradient-to-b from-white w-full h-40 absolute top-0"></div>
         <div className="bg-gradient-to-t from-white w-full h-40 absolute bottom-0"></div>
-        <div className="container relative">
-          <div className="mx-auto max-w-2xl py-20 lg:max-w-4xl lg:px-12 text-center">
-            <div className="flex flex-col gap-4 items-center">
-              <div className=" text-md leading-6 prose uppercase">
-                A starter template for
-              </div>
-              <h1 className="font-display text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter text-black">
-                <Link className="text-red-500 " href="https://sanity.io/">
-                  Sanity
-                </Link>{" "}
-                +{" "}
-                <Link className="text-[#000] " href="https://nextjs.org/">
-                  Next.js
-                </Link>
-              </h1>
-            </div>
+        <div className="relative h-full">
+          <div className="h-full mx-auto max-w-2xl py-20 lg:max-w-4xl lg:px-12 text-center flex items-center justify-center">
             <div className="mt-6 space-y-6 prose sm:prose-lg md:prose-xl lg:prose-2xl text-gray-700">
               <p>
                 This starter is a statically generated site that uses Next.js
@@ -34,25 +18,6 @@ export default async function Page() {
                 collaboration, instant side-by-side content previews, and
                 intuitive editing.
               </p>
-            </div>
-            <div className="flex items-center flex-col gap-4">
-              <GetStartedCode />
-              <Link
-                href="https://www.sanity.io/docs"
-                className="inline-flex text-red-500 text-xs md:text-sm underline hover:text-gray-900"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Sanity Documentation
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  className="w-4 h-4 ml-1 inline"
-                  fill="currentColor"
-                >
-                  <path d="M10 6V8H5V19H16V14H18V20C18 20.5523 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V7C3 6.44772 3.44772 6 4 6H10ZM21 3V12L17.206 8.207L11.2071 14.2071L9.79289 12.7929L15.792 6.793L12 3H21Z"></path>
-                </svg>
-              </Link>
             </div>
           </div>
         </div>
