9152abd4658e9d5bc2f160e40d4b21195fe4e6b4
simongowing1 <simongowing1@gmail.com>
2025-01-29 15:36:14 +0100

    style: text not-found

--- nextjs-app/app/not-found.tsx
+++ nextjs-app/app/not-found.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
+import { twMerge } from 'tailwind-merge'
 
 export default function NotFound() {
   return (
-    <div className='h-dvh w-full flex flex-col items-center justify-center'>
+    <div className={twMerge('h-dvh w-full flex flex-col items-center justify-center', 'sm:text-lg md:text-xl lg:text-2xl text-gray-700')}>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/" className='hover:opacity-50'>Return Home</Link>
