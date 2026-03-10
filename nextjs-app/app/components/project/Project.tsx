f056ecf056be73e502966a5605a2c0017b5e9753
simongowing1 <simongowing1@gmail.com>
2026-03-10 15:39:04 +0100

    fix: update Project component to conditionally render iframe or image and adjust height styling

--- nextjs-app/app/components/project/Project.tsx
+++ nextjs-app/app/components/project/Project.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Project as ProjectType } from "@/sanity.types";
-import { useEffect, useRef } from "react";
 import SanityImage from "../global/SanityImage";
 import ButtonPill from "../global/ButtonPill";
 import gsap from 'gsap';
@@ -19,11 +18,13 @@ const Project = ({ project, isNotFinalProjectInArray = false }: ProjectProps) =>
   const { _id, title, url, slug, coverImage } = project;
 
   return (
-    <div key={_id} className={twMerge("w-screen flex-shrink-0 overflow-hidden relative z-0", 'h-screen')}>
-      <div className="h-full w-full">
-        <SanityImage image={coverImage} wrapperClassName="h-full w-full" priority />
+    <div key={_id} className={twMerge("w-screen flex-shrink-0 overflow-hidden relative z-0", 'h-dvh lg:h-screen')}>
+      <div className="h-full w-full relative">
+        {url ?
+          <iframe src={url} className="h-full w-full pointer-events-none" /> :
+          <SanityImage image={coverImage} wrapperClassName="h-full w-full absolute top-0 left-0" priority />}
       </div>
-      <div className="absolute bottom-0 w-full h-1/3 bg-gradient-to-t from-black flex items-end">
+      <div className="absolute bottom-0 w-full h-1/3 bg-gradient-to-t from-gray-50 flex items-end">
         <div className="container py-10 sm:px-6 flex justify-center sm:justify-end">
           <ButtonPill label={title} href={url} classNameText="text-lg sm:text-2xl" styling="cyan" classNameWrapper="px-6 py-3 text-center" />
         </div>
