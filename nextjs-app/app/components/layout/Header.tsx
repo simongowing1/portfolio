4a719e657b49f6b939f95b934681282084ca7b16
simongowing1 <simongowing1@gmail.com>
2026-03-10 14:06:26 +0100

    style: header w-screen

--- nextjs-app/app/components/layout/Header.tsx
+++ nextjs-app/app/components/layout/Header.tsx
@@ -7,41 +7,41 @@ type HeaderProps = {
   settings: SettingsQueryResult;
 }
 
-export default function Header({settings}: HeaderProps) {
+export default function Header({ settings }: HeaderProps) {
 
   const cvDownload = settings?.cvDownload || '#';
 
   return (
     <>
-    <HeaderClient/>
-    <header id={'site-header'} className="fixed z-50 h-36 sm:h-24 inset-0 flex items-center backdrop-blur-lg transition-[top] duration-500 bg-gradient-to-b from-white/80">
-      <div className="container py-6 sm:px-6">
-        <div className="flex items-center justify-between gap-5">
-          <Link className="flex items-center gap-2" href="/">
-            <span className="text-4xl font-bold pl-2 leading-tight tracking-tighter">
-              Simon Gowing
-            </span>
-          </Link>
+      <HeaderClient />
+      <header id={'site-header'} className="fixed z-50 h-36 sm:h-24 inset-0 flex items-center backdrop-blur-lg transition-[top] duration-500 bg-gradient-to-b from-white/80 w-screen">
+        <div className="container py-6 sm:px-6">
+          <div className="flex items-center justify-between gap-5">
+            <Link className="flex items-center gap-2" href="/">
+              <span className="text-4xl font-bold pl-2 leading-tight tracking-tighter">
+                Simon Gowing
+              </span>
+            </Link>
 
-          <nav className="">
-            <ul
-              role="list"
-              className="flex items-center gap-4 md:gap-6 leading-5 text-sm md:text-base tracking-tight font-normal"
-            >
-              <li>
-                <Link href={cvDownload} download target="_blank" className="">
-                  CV
-                </Link>
-              </li>
+            <nav className="">
+              <ul
+                role="list"
+                className="flex items-center gap-4 md:gap-6 leading-5 text-sm md:text-base tracking-tight font-normal"
+              >
+                <li>
+                  <Link href={cvDownload} download target="_blank" className="">
+                    CV
+                  </Link>
+                </li>
 
-              <li className="sm:before:w-[1px] sm:before:bg-gray-100 before:block flex sm:gap-4 md:gap-6">
-              <ButtonGitHub/>
-              </li>
-            </ul>
-          </nav>
+                <li className="sm:before:w-[1px] sm:before:bg-gray-100 before:block flex sm:gap-4 md:gap-6">
+                  <ButtonGitHub />
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
     </>
   );
 }
